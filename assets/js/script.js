4edbd084d811d8bb5a651aea392d026ebe3c59c4
LeonardoMarson <leonardomateusmarson@gmail.com>
2022-12-11 17:33:02 -0300

    Iniciado o desenvolvimento da responsividade
    Foi adicionado um CSS com o elemento media e junto a ele esta sendo formulada as adaptacoes para o site em dispositivo movel

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,4 +52,16 @@ window.addEventListener('click', () => {
     }
 });
 
+// window.addEventListener("scroll", getScrollY);
+// function getScrollY(){
+//     let scrollY = window.scrollY;
+    
+//     if(scrollY > 40){
+//         header.style.cssText = "height: 15rem; position: fixed; transition: height 0.1s linear; z-index: 1;";
+//     }
+//     else {
+//         header.style.cssText = "height: 21rem; transition: height 0.3s ease;";
+//     }
+// }
+
 
